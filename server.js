5d162bf57d03c448b7880eb90f40becc95a9f144
Ubuntu <ubuntu@ip-172-31-46-193.eu-west-1.compute.internal>
2020-08-04 11:09:48 +0000

    now with immigrants

--- server.js
+++ server.js
@@ -31,6 +31,26 @@ fs.readFile('salaries.txt', function(err, data) {
 
     });
   });
+  
+fs.readFile('immigrant.txt', function(err, data) {
+    if (err) throw err;
+    data = data.toString().split("\n")
+    console.log(data)
+    fs.writeFile('newImmigrant.txt', '', function (err) {
+        if (err) throw err;
+        for (var i = 0; i < data.length; i++){
+            if (data[i].indexOf("invandrar") > -1){
+                var old = parseInt(data[i+1].split(".")[0])+(parseInt(data[i+1].split(".")[1])/100)
+                var now = (old * 1.0124).toFixed(2)
+                console.log(old, now, now/old);
+                fs.appendFile('newImmigrant.txt', data[i] + "\t" + old + "\t" + now + "\n", function (err){
+                    if (err) throw err;
+                });
+            }
+        }
+    });
+    
+});
 
 //var express = require('express')
 //express().use(express.static(process.cwd() + '/public')).listen(8080, function(){console.log("Shortest server ever listening")});
